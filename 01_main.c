ae27f4547c3e38cfb111fc913e5552583881d998
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-07-31 15:01:45 +0530

    base

--- 01_main.c
+++ 01_main.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
 
+#include "01_main.h"   //Include Format	Used For	Searches Where?
+/*#include "..."	Local/project files	
+Current directory, then system
+#include <...>	System headers	Only system paths
+*/
 
-
-
-
-int main(){
-	
-	return 0;}
+int main(void){
+		
+	printf("HELLO");
+	return EXIT_SUCCESS; //macro , this means #define EXIT_SUCCESS 0;
+}
