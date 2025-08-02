632b498ab9791e9a563514172190bea6ace79333
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-08-02 19:09:48 +0530

    added ANSI codes

--- 01_main.h
+++ 01_main.h
@@ -6,4 +6,33 @@
 #include <stdio.h>
 
 
+/* ANSI Color Codes for Terminal Output Formatting (Short Forms)
+
+** X  - Reset        → "\033[0m"
+** R  - Red          → "\033[31m"
+** G  - Green        → "\033[32m"
+** Y  - Yellow       → "\033[33m"
+** B  - Blue         → "\033[34m"
+** M  - Magenta      → "\033[35m"
+** C  - Cyan         → "\033[36m"
+** W  - White        → "\033[37m"
+** BD - Bold         → "\033[1m"
+** UL - Underline    → "\033[4m"
+*/
+
+
+#define X   "\033[0m"
+#define R   "\033[31m"
+#define G   "\033[32m"
+#define Y   "\033[33m"
+#define B   "\033[34m"
+#define M   "\033[35m"
+#define C   "\033[36m"
+#define W   "\033[37m"
+#define BD  "\033[1m"
+#define UL  "\033[4m"
+
+
+
+
 #endif
