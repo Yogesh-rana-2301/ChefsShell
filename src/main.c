622707b38453e378ffcd1748aacda58f73cd6c80
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-11-24 20:52:05 +0530

    added ECHO TYPE builtin commands

--- src/main.c
+++ src/main.c
@@ -16,8 +16,29 @@ int main(int argc, char *argv[]) {
 
     line[strcspn(line, "\n")] = 0;  // Remove newline character
 
+    // EXIT
     if (strcmp(line, "exit 0") == 0 || strcmp(line, "exit 1") == 0 || strcmp(line, "exit") == 0) {
       exit(0);
+    }
+
+    // ECHO
+    else if (strncmp(line, "echo ", 5) == 0) {
+      printf("%s\n", line + 5);
+      continue;
+    } else if (strcmp(line, "echo") == 0) {
+      printf("\n");
+      continue;
+    }
+
+    else if (strncmp(line, "type ", 5) == 0) {
+      if (strcmp(line + 5, "echo") == 0 || strcmp(line + 5, "exit") == 0 ||
+          strcmp(line + 5, "type") == 0) {
+        printf("%s is a shell builtin\n", line + 5);
+        continue;
+      } else {
+        printf("%s: not found\n", line + 5);
+        continue;
+      }
     }
 
     printf("%s: command not found\n", line);
