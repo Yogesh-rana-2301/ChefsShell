9838d8a070e82fd916e3fb151652c5fdea2a0e3f
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-11-25 01:26:29 +0530

    added executable for path

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -30,15 +34,43 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
+    // TYPE
     else if (strncmp(line, "type ", 5) == 0) {
-      if (strcmp(line + 5, "echo") == 0 || strcmp(line + 5, "exit") == 0 ||
-          strcmp(line + 5, "type") == 0) {
-        printf("%s is a shell builtin\n", line + 5);
-        continue;
-      } else {
-        printf("%s: not found\n", line + 5);
+      char *cmd = line + 5;
+      // check for builtins
+      if (strcmp(cmd, "echo") == 0 || strcmp(cmd, "exit") == 0 || strcmp(cmd, "type") == 0) {
+        printf("%s is a shell builtin\n", cmd);
         continue;
       }
+      // check for executable in PATH
+      else {
+        char *path = getenv("PATH");
+        if (path == NULL) path = "";
+        char path_cpy[1000];
+        strncpy(path_cpy, path, sizeof(path_cpy));
+        path_cpy[sizeof(path_cpy) - 1] = '\0';  // Ensure null-termination
+
+        char *dir = strtok(path_cpy, ":");
+
+        int found = 0;
+
+        while (dir != NULL) {
+          char fullpath[1200];
+          snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, cmd);
+
+          if (access(fullpath, X_OK) == 0) {
+            printf("%s is %s\n", cmd, fullpath);
+            found = 1;
+            break;
+          }
+          dir = strtok(NULL, ":");
+        }
+        if (!found) {
+          printf("%s: not found\n", cmd);
+        }
+        continue;
+      } 
+      
     }
 
     printf("%s: command not found\n", line);
